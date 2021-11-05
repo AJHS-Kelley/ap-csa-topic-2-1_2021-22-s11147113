cb0f320b62dc69c6566419ac15dbc75d12651330
Josiah W <70596172+s11147113@users.noreply.github.com>
2021-11-05 08:48:04 -0400

    Create assignLunch Method
    Added different lunch with grade level

---
+++ Student.java
@@ -0,0 +1,97 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Parameters and Arguments
+    public static void assignLunch(int gradeLevel) {
+        String lunchPeriod;
+
+        if (gradeLevel == 9) {
+            lunchPeriod = "First Lunch";
+            System.out.print("This student has ");
+            System.out.println(lunchPeriod);
+        } else if (gradeLevel == 10) {
+            lunchPeriod = "Second Lunch";
+            System.out.print("This student has ");
+            System.out.println(lunchPeriod);
+        } else if (gradeLevel == 11) {
+            lunchPeriod = "Third Lunch";
+            System.out.print("This student has ");
+            System.out.println(lunchPeriod);
+        } else {
+            lunchPeriod = "Off Campus Lunch";
+            System.out.print("This student has ");
+            System.out.println(lunchPeriod);
+        }
